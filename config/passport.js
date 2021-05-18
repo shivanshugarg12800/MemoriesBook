c56ddc3234287b8370df765846b216bbc8f3de58
Shivanshu Garg <shivanshugarg25899@gmail.com>
2021-05-19 00:54:34 +0530

    user saved in DB, logout enabled, header added

--- config/passport.js
+++ config/passport.js
@@ -12,7 +12,25 @@ module.exports = function (passport) {
         callbackURL: "/auth/google/callback",
       },
       async (accessToken, refreshToken, profile, done) => {
-        console.log(profile);
+        //console.log(profile);
+        const newUser = {
+          googleId: profile.id,
+          displayName: profile.displayName,
+          firstName: profile.name.givenName,
+          lastName: profile.name.familyName,
+          image: profile.photos[0].value,
+        };
+        try {
+          let user = await User.findOne({ googleId: profile.id });
+          if (user) {
+            done(null, user);
+          } else {
+            user = await User.create(newUser);
+            done(null, user);
+          }
+        } catch (err) {
+          console.log(err);
+        }
       }
     )
   );
--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const passport = require("passport");
 
 const router = express.Router();
 // DESCRIPTION - auth with google
-// route - GET /auth.google
+// route - GET /auth/google
 // we are only setting up /google here because auth will be called in app.js
 // here we are saying is we  need the scope of whatever is included in the profile
 router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
@@ -18,5 +18,12 @@ router.get(
     res.redirect("/dashboard");
   }
 );
+
+// DESCRIPTION - logout user
+// route   /auth/logout
+router.get("/logout", (req, res) => {
+  req.logout(); // in built method in req of passport
+  res.redirect("/");
+});
 
 module.exports = router;
