8bafe0cf70152f68959a360ee5cc5d13164b567b
Shivanshu Garg <shivanshugarg25899@gmail.com>
2021-05-25 18:12:24 +0530

    project completed

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(
 );
 app.use(express.json());
 
-//-------------METHOD OVERRIDING----------------
+//-------------METHOD OVERRIDING------------------
 app.use(
   methodOverride(function (req, res) {
     if (req.body && typeof req.body === "object" && "_method" in req.body) {
